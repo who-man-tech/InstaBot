800767b22e3f179d644e9aa40a305fc8a63fe6d0
Enes <enes515254@gmail.com>
2020-05-31 16:47:42 +0600

    Update

--- main.py
+++ main.py
@@ -4,35 +4,35 @@ from instapy import InstaPy
 
 def main():
 
-    tags = ["cars", "photo", "bmw", "fitness", "drift", "moto", "training", "food"]
+    tags = ["cars", "photo", "bmw", "fitness", "drift", "moto", "training", "food", "nature", "insta", "happy"]
     comments = ["Nice!", "Cool!", "This is unreal!!!"]
 
 
-    session = InstaPy(username="+77029317645", password="enes51525354", want_check_browser=False)
+    session = InstaPy(username="_who.man_", password="enes51525354", want_check_browser=False)
     # Запуск без графического интерфейса
-    #session = InstaPy(username='test', password='test', headless_browser=True)
+    # session = InstaPy(username='test', password='test', headless_browser=True)
 
     # Вход
     session.login()
     # Устанавливаем ограничение дейтвий в день, час
     session.set_quota_supervisor(enabled=True, peak_comments_daily=240, peak_comments_hourly=21)
     # Устанавливаем задержки для действий в секундах
-    session.set_action_delays(True, like=3, comment=5, follow=5, unfollow=5, story=10)
+    # session.set_action_delays(True, like=3, comment=5, follow=5, unfollow=5, story=10)
     # Отписываемся от людей, которые не подписались на нас
-    session.unfollow_users(amount=50, nonFollowers=True, style="RANDOM", unfollow_after=42 * 60 * 60, sleep_delay=655)
     # Устанавливаем ограничение на аккаунты у которых больше 8500 подписчиков
     session.set_relationship_bounds(enabled=True, max_followers=4000)
+    # Подписываем на 50% аккаунтов, которым мы поставили лайк
+    session.set_do_follow(True, percentage=50)
+    # Оставляем комментарии на 50% аккаунтов, которым мы поставили лайк
+    session.set_comments(comments)
+    session.set_do_comment(True, percentage=50)
 
     # Отписываемся от аккаунтов на которые подписался бот, но они нас не подписались
     session.unfollow_users(amount=60, instapy_followed_enabled=True, instapy_followed_param="all", style="FIFO", unfollow_after=(60 * 60 * 24), sleep_delay=501)
     # Ставим лайки по хештегам
     session.like_by_tags(tags, amount=2)
     # session.set_dont_like(["naked", "nsfw"])
-    # Подписываем на 50% аккаунтов, которым мы поставили лайк
-    session.set_do_follow(True, percentage=50)
-    # Оставляем комментарии на 50% аккаунтов, которым мы поставили лайк
-    session.set_comments(comments)
-    session.set_do_comment(True, percentage=50)
+
     
 
     session.end()
